d3c306064ed11f7dcc72f9bc834628e814fb249c
cupsforbreakfast <weblogaisa@mail.ru>
2017-12-09 17:16:23 +0300

    My Django Girls app, first commit

---
+++ testing/pr.py
@@ -0,0 +1,8 @@
+def crop(a):
+	if a<=20:
+		print('Nice crops')
+	else:
+		print('Bad crops')
+
+
+crop(a=1000)
---
+++ testing/pr1.py
@@ -0,0 +1,8 @@
+def eat (name):
+	print('It\'s breakfast time,' + name + '!')
+
+
+cats = ['Myashka', 'Boris', 'Kolya', 'Murzik']
+for name in cats:
+	eat (name)
+	print('Next')
